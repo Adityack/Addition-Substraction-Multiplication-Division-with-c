1d9041a9bec96968fff1bb894c4f635631c8d506
Adi1ya <adityamaji474@gmail.com>
2022-02-19 22:48:06 +0530

    Add files via upload

---
+++ Addition subtraction multiplication and division.c
@@ -0,0 +1,27 @@
+/*******************************************
+Statement - Addition, Substraction, Multiplication, Division
+Programmer - Aditya Maji
+Written For - need2know.online
+********************************************/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,addition,substraction,multiplication;
+    float division;
+    clrscr();
+    printf("Enter The Value Of a:");
+    scanf("%d",&a);
+    printf("enter the value of b:");
+    scanf("%d",&b);
+    addition=a+b;
+    substraction=a-b;
+    multiplication=a*b;
+    division=a/b;
+    printf("The Addition of a b is %d",addition);
+    printf("The Addition of a b is %d",substraction);
+    printf("The Addition of a b is %d",multiplication);
+    printf("The Addition of a b is %f",division);
+    getch();
+}
+//
